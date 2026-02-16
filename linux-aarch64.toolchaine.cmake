276ec3a8f0c9b8324fa803d93dedd8079eb1a4b1
Sam4uk <sam4uk.site@gmail.com>
2026-02-16 11:10:27 +0200

    16/02/26 11:10:27

--- linux-aarch64.toolchaine.cmake
+++ linux-aarch64.toolchaine.cmake
@@ -1,6 +1,5 @@
 # Вказуємо, що це крос-компіляція під Linux
 set(CMAKE_SYSTEM_NAME Linux)
-
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 
@@ -11,6 +10,8 @@ set(CMAKE_SYSTEM_PROCESSOR armv8)
 # Вказуємо компілятор для крос-компіляції
 set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
+set(CMAKE_AR /usr/bin/aarch64-linux-gnu-ar)
+set(CMAKE_LINKER /usr/bin/aarch64-linux-gnu-ld)
 
 # # set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu) # Adjust this path if needed
 # # # Корінь системи для aarch64 (якщо встановлено sysroot)
@@ -30,7 +31,6 @@ set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
 
 # Шлях до утиліт binutils (за потреби)
 # set(CMAKE_ASM_COMPILER /usr/bin/aarch64-linux-gnu-as)
-set(CMAKE_LINKER /usr/bin/aarch64-linux-gnu-ld)
 # set(CMAKE_OBJCOPY /usr/bin/aarch64-linux-gnu-objcopy)
 # set(CMAKE_OBJDUMP /usr/bin/aarch64-linux-gnu-objdump)
 # set(CMAKE_RANLIB /usr/bin/aarch64-linux-gnu-ranlib)
@@ -40,16 +40,15 @@ set(CMAKE_LINKER /usr/bin/aarch64-linux-gnu-ld)
 set(CMAKE_CROSSCOMPILING TRUE)
 
 
+set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
+
 # Шукати бібліотеки лише в кореневому каталозі
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-
-
 # set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
-
 
 # Вказуємо кореневий каталог для Linux (де знаходяться бібліотеки та інструменти)
 # set(CMAKE_FIND_ROOT_PATH /usr/x86_64-linux-gnu)
